ecfb816aec403b1ac2547023a2b6a809e63d3f90
jpalacioco <50222427+jpalacioco@users.noreply.github.com>
2019-06-07 17:08:38 -0500

    Laboratorio 2
    Terminado

--- script.sh
+++ script.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Laboratorio No 2
 
+x=("data1.csv" "data2.csv" "data3.csv")
+
+for n in ${x[*]}
+do
+  i=1
+  while read line; do
+    # reading each line
+    lineai=($line)
+    #echo "$i ${lineai[1]}"
+    for ii in $(echo ${lineai[1]} | tr "," "\n")
+      do
+       echo $n,$i,${lineai[0]},$ii
+      done
+
+    i=$((i+1))
+  done < $n
+done
