343e7b93bf27e010c471b667c694a21dbc5c5014
jpalacioco <50222427+jpalacioco@users.noreply.github.com>
2019-06-08 11:09:22 -0500

    Laboratorio 2
    Terminado

--- script.sh
+++ script.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # Laboratorio No 2
 
-x=("data1.csv" "data2.csv" "data3.csv")
+archivos=("data1.csv" "data2.csv" "data3.csv")
 
-for n in ${x[*]}
+for archivo in ${archivos[*]}     # Iterador para los archivos
 do
-  i=1
-  while read line; do
+  i=1                             # Inicio de contador para cada linea
+  while read linea; do            # Lectura de la linea
+  
     # reading each line
-    lineai=($line)
-    #echo "$i ${lineai[1]}"
-    for ii in $(echo ${lineai[1]} | tr "," "\n")
+    linea_actual=($linea)         # Linea como arreglo
+    linea_letra=${linea_actual[0]}  # Letra en la linea
+    linea_texto=${linea_actual[1]}  # Texto en la linea
+      for elemento in $(echo $linea_texto | tr "," "\n") # Similar a hacer el split por ,
       do
-       echo $n,$i,${lineai[0]},$ii
+       echo $archivo,$i,${linea_letra},$elemento
       done
-
-    i=$((i+1))
-  done < $n
+    if [ "$linea_actual" != "" ]; then # Si la linea esta vacia no aumenta el contador
+        i=$((i+1))
+    fi
+  done < <(grep "" $archivo) # Se usa el grep para que lea todas
+                             # las lineas. 
+  i=0                        # reiniciar el contador de lineas
 done
